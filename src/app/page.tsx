97f8d40326b7739ba610aca6aa31244bebe90a38
solar <nathf1005@naver.com>
2025-03-24 00:51:02 +0900

    fix: prettier 정렬

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,23 +7,23 @@ export default function Home() {
       <input
         type="text"
         placeholder="아이디를 입력하세요"
-        className="mt-30 w-full p-3 border text-sm border-gray-300 rounded-md focus:outline-none focus:ring-1 focus:ring-gray-500"
+        className="mt-30 w-full rounded-md border border-gray-300 p-3 text-sm focus:ring-1 focus:ring-gray-500 focus:outline-none"
       />
       <input
         type="text"
         placeholder="비밀번호를 입력하세요"
-        className="mt-5 w-full p-3 border text-sm border-gray-300 rounded-md focus:outline-none focus:ring-1 focus:ring-gray-500"
+        className="mt-5 w-full rounded-md border border-gray-300 p-3 text-sm focus:ring-1 focus:ring-gray-500 focus:outline-none"
       />
       <p className="mt-5 text-sm">자동로그인</p>
-      <button className="mt-20 w-full p-3 border text-sm border-gray-300 rounded-md">
+      <button className="mt-20 w-full rounded-md border border-gray-300 p-3 text-sm">
         로그인
       </button>
-      <div className="mt-3 flex gap-2 justify-center">
-        <p className="text-sm cursor-pointer">ID/PW 찾기</p>
+      <div className="mt-3 flex justify-center gap-2">
+        <p className="cursor-pointer text-sm">ID/PW 찾기</p>
         <p className="text-sm">|</p>
-        <p className="text-sm cursor-pointer">ID/PW 찾기</p>
+        <p className="cursor-pointer text-sm">ID/PW 찾기</p>
       </div>
-      <button className="mt-3 w-full p-3 border text-sm border-gray-300 rounded-md">
+      <button className="mt-3 w-full rounded-md border border-gray-300 p-3 text-sm">
         카카오로 로그인
       </button>
     </div>
