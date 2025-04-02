f80dc12af722dd4f087d2e6495be53421dd8adb9
solar <nathf1005@naver.com>
2025-04-03 00:40:16 +0900

    feat: 홈으로 이동 기능

--- src/app/(components)/Header.tsx
+++ src/app/(components)/Header.tsx
@@ -1,11 +1,18 @@
+"use client";
+
+import { useRouter } from "next/navigation";
 import { BsFillTicketPerforatedFill } from "react-icons/bs";
 import { IoMdMenu } from "react-icons/io";
 
 export default function Header() {
+  const router = useRouter();
+
   return (
     <div className="mx-auto max-w-6xl px-5">
       <div className="mt-9 flex items-center justify-between">
-        <p className="text-base">MovieGo</p>
+        <p className="text-base" onClick={() => router.push("/")}>
+          MovieGo
+        </p>
         <div className="flex items-center gap-2">
           <BsFillTicketPerforatedFill size={24} />
           <IoMdMenu size={24} />
