1ee3a42ea71a70e492e50743e326a82dddeff5fb
solar <nathf1005@naver.com>
2025-04-06 19:32:13 +0900

    feat: 사이드메뉴 헤더부분 작업

--- src/app/(components)/SideBar.tsx
+++ src/app/(components)/SideBar.tsx
@@ -1,27 +1,71 @@
 import { IoMdMenu } from "react-icons/io";
+import { RiMovie2Fill } from "react-icons/ri";
 
 import {
   Sheet,
   SheetContent,
-  SheetDescription,
   SheetHeader,
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
+import { FaHome } from "react-icons/fa";
+import { LucideTicket } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 export default function SideBar() {
+  const [isOpen, setIsOpen] = useState(false);
+  const router = useRouter();
+
   return (
-    <Sheet>
+    <Sheet open={isOpen} onOpenChange={setIsOpen}>
       <SheetTrigger>
         <IoMdMenu size={24} />
       </SheetTrigger>
       <SheetContent>
         <SheetHeader>
-          <SheetTitle>Are you absolutely sure?</SheetTitle>
-          <SheetDescription>
-            This action cannot be undone. This will permanently delete your
-            account and remove your data from our servers.
-          </SheetDescription>
+          <SheetTitle>
+            <FaHome
+              size={18}
+              onClick={() => {
+                router.push("/");
+                setIsOpen(false);
+              }}
+            />
+          </SheetTitle>
+          <div>
+            <p className="inline-block border-b border-black py-2 text-black">
+              MENU
+            </p>
+          </div>
+          <div className="mt-5 flex items-center justify-evenly">
+            <div className="text-center">
+              <div className="rounded-md border-2 border-black p-3">
+                <RiMovie2Fill
+                  size={48}
+                  color="black"
+                  onClick={() => {
+                    router.push("/movie");
+                    setIsOpen(false);
+                  }}
+                />
+              </div>
+              <p className="mt-1">영화조회</p>
+            </div>
+            <div className="text-center">
+              <div className="rounded-md border-2 border-black p-3">
+                <LucideTicket
+                  size={48}
+                  color="black"
+                  onClick={() => {
+                    router.push("/reserve");
+                    setIsOpen(false);
+                  }}
+                />
+              </div>
+              <p className="mt-1">바로예매</p>
+            </div>
+          </div>
         </SheetHeader>
       </SheetContent>
     </Sheet>
