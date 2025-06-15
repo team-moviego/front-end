f8b04b9bb810e4731c551323b51f2843371f573b
solar <nathf1005@naver.com>
2025-06-15 19:03:53 +0900

    fix: 예매 정보 섹션 레이아웃 개선 및 그리드로 변경

--- src/app/(pages)/ticket/complete/[id]/page.tsx
+++ src/app/(pages)/ticket/complete/[id]/page.tsx
@@ -17,38 +17,22 @@ export default function TicketCompletePage() {
         </section>
 
         <section className="mt-25">
-          <p className="text-xl font-bold">예매정보</p>
-          <div className="mt-5 flex justify-between">
-            <p className="text-gray-500">예매번호</p>
-            <p>1234567890</p>
-          </div>
-          <div className="mt-5 flex justify-between">
-            <p className="text-gray-500">예매자</p>
-            <p>홍길동</p>
-          </div>
-          <div className="mt-5 flex justify-between">
-            <p className="text-gray-500">예매일</p>
-            <p>2025-01-01</p>
-          </div>
-          <div className="mt-5 flex justify-between">
-            <p className="text-gray-500">영화</p>
-            <p>영화제목</p>
-          </div>
-          <div className="mt-5 flex justify-between">
-            <p className="text-gray-500">관람일시</p>
-            <p>2025-01-01 12:00</p>
-          </div>
-          <div className="mt-5 flex justify-between">
-            <p className="text-gray-500">영화명</p>
-            <p>영화제목</p>
-          </div>
-          <div className="mt-5 flex justify-between">
-            <p className="text-gray-500">상영관</p>
-            <p>상영관</p>
-          </div>
-          <div className="mt-5 flex justify-between">
-            <p className="text-gray-500">좌석</p>
-            <p>좌석</p>
+          <p className="mb-6 text-xl font-bold">예매정보</p>
+          <div className="grid grid-cols-2 gap-4">
+            <div className="text-gray-500">예매번호</div>
+            <div>1234567890</div>
+            <div className="text-gray-500">예매자</div>
+            <div>홍길동</div>
+            <div className="text-gray-500">예매일</div>
+            <div>2025-01-01</div>
+            <div className="text-gray-500">영화</div>
+            <div>영화제목</div>
+            <div className="text-gray-500">관람일시</div>
+            <div>2025-01-01 12:00</div>
+            <div className="text-gray-500">상영관</div>
+            <div>1관</div>
+            <div className="text-gray-500">좌석</div>
+            <div>A1, A2</div>
           </div>
         </section>
       </div>
