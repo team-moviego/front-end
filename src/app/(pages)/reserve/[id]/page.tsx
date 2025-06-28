291a1efa57ae3b3947a0eed4e6c04d082f666e44
solar <nathf1005@naver.com>
2025-06-28 20:25:02 +0900

    fix: 예약 페이지 컴포넌트 경로변경

--- src/app/(pages)/reserve/[id]/page.tsx
+++ src/app/(pages)/reserve/[id]/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import Footer from "../../(components)/Footer";
-import Header from "../../(components)/Header";
+import Footer from "@/app/(components)/Footer";
+import Header from "@/app/(components)/Header";
 import {
   Drawer,
   DrawerClose,
